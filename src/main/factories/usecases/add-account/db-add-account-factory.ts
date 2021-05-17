43aff66bf5f77d653cbad3045abd9abc47bd0693
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2021-05-17 19:06:16 -0300

    feat: add AddSurvey composition

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -1,11 +1,8 @@
-import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '../../../../infra/criptography/bcrypter-adapter/bcrypt-adapter'
-import { AddAccount } from '../../../../domain/usecases/add-account'
+import { SurveyMongoRepository } from '../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { AddSurvey } from '../../../../domain/usecases/add-survey'
+import { DbAddSurvey } from '../../../../data/usecases/add-survey/db-add-survey'
 
-export const makeDbAddAccount = (): AddAccount => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
+export const makeDbAddSurvey = (): AddSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbAddSurvey(surveyMongoRepository)
 }
