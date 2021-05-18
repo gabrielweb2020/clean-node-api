0bccb52b375dba693f0af2e7b51a19ce421180e6
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2021-05-18 16:15:33 -0300

    feat: add middleware interface

--- src/main/factories/usecases/add-survey/db-add-survey-factory.ts
+++ src/main/factories/usecases/add-survey/db-add-survey-factory.ts
@@ -1,11 +1,9 @@
-import { DbAddAccount } from '../../../../data/usecases/add-account/db-add-account'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '../../../../infra/criptography/bcrypter-adapter/bcrypt-adapter'
-import { AddAccount } from '../../../../domain/usecases/add-account'
 
-export const makeDbAddAccount = (): AddAccount => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const accountMongoRepository = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoRepository, accountMongoRepository)
+import { SurveyMongoRepository } from '../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { AddSurvey } from '../../../../domain/usecases/add-survey'
+import { DbAddSurvey } from '../../../../data/usecases/add-survey/db-add-survey'
+
+export const makeDbAddSurvey = (): AddSurvey => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  return new DbAddSurvey(surveyMongoRepository)
 }
