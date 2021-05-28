48752202b85b977e1f3085f6d9a69dd42e97baae
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2021-05-28 20:50:42 -0300

    refactor: rename argument models sufix from Model to Params

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,13 +1,9 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
-import { SaveSurveyResult } from '@/domain/usecases/survey-result/save-survey-result'
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden } from '@/presentation/helpers/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (
-    private readonly loadSurveyById: LoadSurveyById,
-    private readonly saveSurveyResult: SaveSurveyResult
-  ) {}
+  constructor (private readonly loadSurveyById: LoadSurveyById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
